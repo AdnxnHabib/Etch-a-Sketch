666eed285dcf92423218157602fa8152434259f0
ADNXNHABIB <adnanhabib263@gmail.com>
2020-05-19 11:54:04 -0400

    created grid and added mouseover function

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-container = document.getElementsByClassName("container");
+let containerDiv = document.querySelector(".container");
+let i;
+let j;
+let ColumnDiv;
+let CellDiv;
+let buttonReset;
+let k;
+ buttonReset = document.createElement('button');
+buttonReset.classList.add('reset');
+buttonReset.innerHTML = "Reset Grid";
+containerDiv.appendChild(buttonReset);
+
+//Clears Grid 
+
+
+
+makeGrid();
+
+function makeGrid()
+{
+    for( i = 0; i < 16; i++)
+    {
+        
+         ColumnDiv = document.createElement('div');
+         ColumnDiv.classList.add('column')
+         containerDiv.appendChild(ColumnDiv)
+         for(j = 0; j < 16; j++ )
+         {
+            CellDiv = document.createElement('div');
+            CellDiv.classList.add('cell')
+            ColumnDiv.appendChild(CellDiv);
+            CellDiv.addEventListener("mouseover", function(){
+                this.style.backgroundColor = "red";
+
+            } )
+         
+            
+
+
+         }
+    
+
+        
+
+    }
+
+}
+
+console.log(document.body.childNodes)
+
+buttonReset.addEventListener('click', function(){
+
+   
+
+})
+
+
