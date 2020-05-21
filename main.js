8c26e491b6662dccae0de5846e023395f387def0
ADNXNHABIB <adnanhabib263@gmail.com>
2020-05-21 14:07:35 -0400

    added a grid, using flex and maintained the fixed size

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 let containerDiv = document.querySelector(".container");
+
+
+
 let i;
 let j;
 let ColumnDiv;
 let CellDiv;
 let buttonReset;
 let k;
- buttonReset = document.createElement('button');
+let cells;
+var girdSize;
+
+grid();
+
+function grid(){
+    gridSize = prompt("Please enter the intial grid size please");
+}
+
+
+let container2 = document.createElement('div');
+container2.classList.add('container2');
+document.body.appendChild(container2);
+
+
+buttonReset = document.createElement('button');
 buttonReset.classList.add('reset');
 buttonReset.innerHTML = "Reset Grid";
-containerDiv.appendChild(buttonReset);
+container2.appendChild(buttonReset);
+
+const getWidth = function(targetElement) {
+
+    const style = window.getComputedStyle(targetElement);
+    const widthStr = style.getPropertyValue('width');                //getting '500px';
+    const widthValue = widthStr.replace('px', '');
+
+    return widthValue;                                              //returns string
+}
+
+
+const getHeight = function(targetElement) {
+
+    const style = window.getComputedStyle(targetElement);
+    const heightStr = style.getPropertyValue('height');                //getting '500px';
+    const heightValue = heightStr.replace('px', '');
+
+    return heightValue;                                                 //returns string
+}
 
 //Clears Grid 
 
@@ -16,19 +53,25 @@ containerDiv.appendChild(buttonReset);
 
 makeGrid();
 
+
 function makeGrid()
 {
-    for( i = 0; i < 16; i++)
+    for( i = 0; i < gridSize; i++)
     {
         
-         ColumnDiv = document.createElement('div');
-         ColumnDiv.classList.add('column')
-         containerDiv.appendChild(ColumnDiv)
-         for(j = 0; j < 16; j++ )
+         rowDiv = document.createElement('div');
+         rowDiv.classList.add('row')
+        
+        rowDiv.style.height = `${getWidth(containerDiv)/gridSize}px`;
+         containerDiv.appendChild(rowDiv)
+         for(j = 0; j < gridSize; j++ )
          {
             CellDiv = document.createElement('div');
             CellDiv.classList.add('cell')
-            ColumnDiv.appendChild(CellDiv);
+            CellDiv.style.width = `${getHeight(containerDiv)/gridSize}px`;
+
+            
+            rowDiv.appendChild(CellDiv);
             CellDiv.addEventListener("mouseover", function(){
                 this.style.backgroundColor = "red";
 
@@ -46,11 +89,18 @@ function makeGrid()
 
 }
 
-console.log(document.body.childNodes)
+
+
+
 
 buttonReset.addEventListener('click', function(){
 
-   
+
+   cells = document.getElementsByClassName('cell');
+   for(k = 0; k < cells.length; k++)
+   {
+       cells[k].style.backgroundColor = "white";
+   }    
 
 })
 
